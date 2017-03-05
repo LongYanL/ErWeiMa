28a3c338b72efe420c8a8e41fa85e46abb1c0e52
liyanlong986829 <526970264@qq.com>
2017-03-05 21:12:37 +0800

    Initial commit

---
+++ app/src/main/java/com/baway/administrator/erweima/MApplication.java
@@ -0,0 +1,19 @@
+package com.baway.administrator.erweima;
+
+import android.app.Application;
+
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+/**
+ * Created by aaron on 16/9/7.
+ */
+
+public class MApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ZXingLibrary.initDisplayOpinion(this);
+    }
+}
